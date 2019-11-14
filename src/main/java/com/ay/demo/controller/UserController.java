40608f4a22de5a6806b5ec18bb2a211ed6e08282
1120965621@qq.com <1120965621@qq.com>
2019-11-14 18:32:58 +0800

    0

--- src/main/java/com/ay/demo/controller/UserController.java
+++ src/main/java/com/ay/demo/controller/UserController.java
@@ -1,10 +1,8 @@
 package com.ay.demo.controller;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,8 +13,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ay.demo.entity.ResponseBean;
 import com.ay.demo.entity.User;
-import com.ay.demo.entity.WebSiteVisitData;
-import com.ay.demo.mapper.UserMapper;
 import com.ay.demo.service.UserService;
 
 /**
@@ -29,8 +25,7 @@ public class UserController {
 	
 	@Autowired
 	private UserService userService;
-	@Autowired
-	private UserMapper userMapper;
+
 	@GetMapping("/getindex")
 	public String index() {
 		return "index";
@@ -50,26 +45,6 @@ public class UserController {
 		long time = System.currentTimeMillis() - startTime;
 		System.out.println(time/1000.0);
 		return responseBean;
-	}
-	
-	@ResponseBody
-	@GetMapping("/token/getVisitNum")
-	public ResponseBean getVisitNum(@RequestParam("url") String url,HttpServletResponse response,HttpServletRequest request) {
-		response.setHeader("Access-Control-Allow-Credentials", "true");
-		response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-		if(WebSiteVisitData.getInstance().visitNum.get() == 0) {
-			int visitNum = userMapper.getVisitNum(url);
-			if(visitNum == 0) {
-				WebSiteVisitData.getInstance().visitNum.getAndSet(1);
-				return ResponseBean.success(1);
-			}
-		}
-		WebSiteVisitData.getInstance().visitNum.incrementAndGet();
-		return ResponseBean.success(WebSiteVisitData.getInstance().visitNum);
-	}
-	@Scheduled(cron="0 */1 * * * ?")
-	public void updateVisitNum() {
-		userMapper.updateVisitNum(WebSiteVisitData.getInstance().visitNum.get());
 	}
 	
 	@RequestMapping("/userInfo")
--- src/main/java/com/ay/demo/service/UserService.java
+++ src/main/java/com/ay/demo/service/UserService.java
@@ -3,7 +3,9 @@ package com.ay.demo.service;
 import com.ay.demo.entity.ResponseBean;
 
 public interface UserService {
-	public ResponseBean login(String phone, String password);
-	public ResponseBean updateAccount(String phone, String money);
+
+	ResponseBean login(String phone, String password);
+
+	ResponseBean updateAccount(String phone, String money);
 	
 }
